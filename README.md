931310cc32fa62bfd6c3aa5f123a7203a6ed8135
eby <eby@broadinstitute.org>
2017-09-06 11:49:23 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+#  Introduction
+
+**Single-sample GSEA (ssGSEA)**, an extension of **Gene Set Enrichment Analysis (GSEA)**, calculates separate enrichment scores for each pairing of a sample and gene set.  Each ssGSEA enrichment score represents the degree to which the genes in a particular gene set are coordinately up- or down-regulated within a sample.  The ssGSEAProjection module for GenePattern is a free genomic analysis program written in the R language implementing this method in a form suitable for [GenePattern](http://www.genepattern.org/).
+
+See the [module documentation](docs/v9/index.html) for tips on usage.
+
+# License
+
+The ssGSEAProjection module for GenePattern is made available under the terms of a BSD-style license, a copy of which is included in the distribution in the [LICENSE.txt](LICENSE.txt) file.  See that file for exact terms and conditions.
+
+#  Latest Version
+
+The latest binary release of this software can be obtained from the GenePattern module repository, available from the Administration screen of the web UI.  It is currently in Beta release as v9.x. 
+
+If you have any comments, suggestions or bugs to report, please see our [Contact page](http://www.gsea-msigdb.org/gsea/contact.jsp) for information on how to reach us.
+
+# History and Acknowledgments
+
+**The ssGSEAProjection module v9** is the open-source release; the initial GitHub commit corresponds to this release of August 7, 2017. The code revision history from the v8 and earlier releases is not available.
+  
+David Eby was responsible for the open-source conversion of the module and handles current maintenance and new feature development.  While David is listed on the initial commit to this public GitHub repository, original authorship is due to the individuals listed in the papers referenced in the [module documentation](docs/v9/index.html), regardless of the GitHub history metadata.
+
+The GSEA project is currently a joint effort of the Broad Institute and the University of California San Diego, and funded by the National Cancer Institute of the National Institutes of Health (PI: JP Mesirov).
+
+# Dependencies
+
+The ssGSEAProjection module is written in 100% pure R.  It has been tested on R-2.5 and R-2.15; running it with other versions of R may be possible but are unsupported. 
+
+------
+Copyright (c) 2012-2017 Broad Institute, Inc., Massachusetts Institute of Technology, and Regents of the University of California.  All rights reserved.
---
+++ src/ssGSEAProjection.R
@@ -0,0 +1,121 @@
+# Copyright (c) 2012-2017 Broad Institute, Inc., Massachusetts Institute of Technology, and Regents of the University of California.  All rights reserved.
+
+# ssGSEA Projection
+
+# processes the cmd line for the ssGSEA.project.dataset
+ssGSEA.projection.cmdline <- function(...)
+{
+    input.gct.filename               <- NA
+    output.prefix                    <- NA
+    gene.sets.db.list.filename       <- NA
+    gene.symbol.column               <- "Name"
+    gene.set.selection               <- "ALL"
+    sample.normalization.method      <- "rank"
+    weighting.exponent               <- 0.75
+    min.overlap                      <- 1
+    combine.mode                     <- "combine.all"
+
+    args <- list(...)
+    for (i in 1:length(args[[1]]))
+    {
+        arg <- args[[1]][i]
+        flag <- substring(arg, 1, 2)
+        value <- substring(arg, 3, nchar(arg))
+
+        if (value == '')
+        {
+            next
+        }
+        else if (flag == '-l')
+        {
+            libdir <- value
+        }
+        else if (flag == '-i')
+        {
+            input.gct.filename <- value
+        }
+        else if (flag == '-o')
+        {
+            output.prefix <- value
+        }
+        else if (flag == '-D')
+        {
+            gene.sets.db.list.filename <- value
+        }
+        else if (flag == '-c')
+        {
+            gene.symbol.column <- value
+        }
+        else if (flag == '-s')
+        {
+            gene.set.selection <- unlist(strsplit(value,','))
+        }
+        else if (flag == '-n')
+        {
+            sample.normalization.method <- value
+        }
+        else if (flag == '-w')
+        {
+            weighting.exponent <- as.numeric(value)
+        }
+        else if (flag == '-v')
+        {
+            min.overlap <- as.integer(value)
+        }
+        else if (flag == '-C')
+            combine.mode <- value
+        else
+            stop("Unknown option", flag)
+
+    }
+
+    if (is.na(input.gct.filename))
+        stop("Missing input.gct.filename")
+
+    if (is.na(output.prefix))
+    {
+        temp <- strsplit(input.gct.filename, split="/") # Extract input file name
+        s <- length(temp[[1]])
+        input.file.name <- temp[[1]][s]
+        temp <- strsplit(input.file.name, split=".gct")
+        output.prefix <-  paste(temp[[1]][1],".PROJ", sep="")
+    }
+
+    gene.sets.dbfile.list <- NA
+
+    if (!is.na(gene.sets.db.list.filename)) {
+        gene.sets.dbfile.list <- readLines(gene.sets.db.list.filename)
+    }
+    else {
+        stop("No Gene Set DB files provided")
+    }
+
+    setup(libdir)
+    source(paste(libdir,"ssGSEAProjection.Library.R", sep=''))
+
+    suppressWarnings(ssGSEA.project.dataset(input.gct.filename,
+                                            paste(output.prefix, ".gct", sep=""),
+                                            gene.sets.dbfile.list = gene.sets.dbfile.list,
+                                            gene.symbol.column = gene.symbol.column,
+                                            gene.set.selection = gene.set.selection,
+                                            sample.norm.type = sample.normalization.method,
+                                            weight = weighting.exponent,
+                                            min.overlap = min.overlap,
+                                            combine.mode = combine.mode))
+}
+
+setup <- function(libdir)
+{
+    source(paste(libdir, "common.R", sep=''))
+    setLibPath(libdir)
+    install.required.packages(libdir)
+}
+
+install.required.packages <- function(libdir)
+{
+    info(libdir)
+    # no non-base packages required by this module
+}
+
+# Call the command-line function, passing the args from GenePattern
+ssGSEA.projection.cmdline(commandArgs(trailingOnly=T))
